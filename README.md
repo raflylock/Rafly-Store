f2373a35e32a8ee59fb953b7b9e2db744fbb2583
raflylock <108665753+raflylock@users.noreply.github.com>
2022-07-04 17:24:48 +0700

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# Rafly-Store-
+# DummyJSON API with Flutter
+
+This repository contains the source code of TOKO ELEKTRONIK, an example of getting data from the DummyJSON API to display in an application using Flutter. To display product lists and detailS.
+
+
+## Demo
+
+Demo of the application.
+
+
+![Logo](https://raw.githubusercontent.com/nurulfurqon18/flutter_dummyjson/main/1.jpg)
+
+![Logo](https://raw.githubusercontent.com/nurulfurqon18/flutter_dummyjson/main/2.jpg)
+
+
+
+## Authors
+
+- Ahmad Nurul Furqon/19.0504.0009
+- [Ahmad Nurul Furqon](https://www.instagram.com/nurulfurqon18/) (Instagram) 
---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,18 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\development\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "D:\\Kuliah\\Semester 6\\Program Mobile\\project\\raflyAAS" PROJECT_DIR)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\development\\flutter"
+  "PROJECT_DIR=D:\\Kuliah\\Semester 6\\Program Mobile\\project\\raflyAAS"
+  "FLUTTER_ROOT=C:\\development\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=D:\\Kuliah\\Semester 6\\Program Mobile\\project\\raflyAAS\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=D:\\Kuliah\\Semester 6\\Program Mobile\\project\\raflyAAS"
+  "FLUTTER_TARGET=D:\\Kuliah\\Semester 6\\Program Mobile\\project\\raflyAAS\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=D:\\Kuliah\\Semester 6\\Program Mobile\\project\\raflyAAS\\.dart_tool\\package_config.json"
+)
